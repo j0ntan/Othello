660570271651ec0c10ae43cb30c99b19740fde70
j0ntan <jgamboa0209@gmail.com>
2018-10-25 03:10:00 -0400

    Count black score for populated Board

--- core/GameState.cpp
+++ core/GameState.cpp
@@ -1,12 +1,25 @@
 #include "GameState.hpp"
 
+namespace {
+const int countTiles(const OthelloBoard &board, OthelloCell tile) noexcept {
+  int counter = 0;
+  for (int x = 0; x < board.width(); ++x)
+    for (int y = 0; y < board.height(); ++y)
+      if (board.cellAt(x, y) == tile)
+        ++counter;
+  return counter;
+}
+} // namespace
+
 GameState::GameState(const Board &board) : reference_board(board) {}
 
 const OthelloBoard &GameState::board() const noexcept {
   return reference_board;
 }
 
-int GameState::blackScore() const noexcept { return 0; }
+int GameState::blackScore() const noexcept {
+  return countTiles(board(), OthelloCell::black);
+}
 
 int GameState::whiteScore() const noexcept {}
 
--- gtest/testGameState.cpp
+++ gtest/testGameState.cpp
@@ -20,3 +20,15 @@ TEST(testGameState, blackScoreIsZeroForEmptyBoard) {
   GameState gameState(board);
   EXPECT_EQ(0, gameState.blackScore());
 }
+
+TEST(testGameState, blackScoreCorrectForPopulatedBoard) {
+  Board board;
+  board.placeTile(0, 0, OthelloCell::black);
+  board.placeTile(1, 1, OthelloCell::black);
+  board.placeTile(2, 2, OthelloCell::black);
+  board.placeTile(3, 3, OthelloCell::black);
+  board.placeTile(4, 4, OthelloCell::white);
+
+  GameState gameState(board);
+  EXPECT_EQ(4, gameState.blackScore());
+}
