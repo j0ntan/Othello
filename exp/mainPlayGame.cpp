d30dccc0e77c7de9b135efd90c44eccf6f4e686d
j0ntan <jgamboa0209@gmail.com>
2018-11-01 03:50:51 -0400

    Announce winner

--- exp/mainPlayGame.cpp
+++ exp/mainPlayGame.cpp
@@ -1,9 +1,11 @@
 #include "core/Board.hpp"
 #include "core/GameState.hpp"
 #include "core/SimpleAI.hpp"
+#include <iostream>
 
 // function prototypes
 Board startingPositionBoard();
+void announceWinner(const GameState &gameState);
 
 int main() {
   Board board = startingPositionBoard();
@@ -13,6 +15,8 @@ int main() {
     // human moves
     // computer moves
   }
+
+  announceWinner(gameState);
 }
 
 Board startingPositionBoard() {
@@ -22,4 +26,14 @@ Board startingPositionBoard() {
   board.setCellAt(3, 4, OthelloCell::black);
   board.setCellAt(4, 4, OthelloCell::white);
   return board;
+}
+
+void announceWinner(const GameState &gameState) {
+  const int final_score = gameState.blackScore() - gameState.whiteScore();
+  if (final_score > 0)
+    std::cout << "Black wins!\n";
+  else if (final_score < 0)
+    std::cout << "White wins!\n";
+  else
+    std::cout << "Tied game!\n";
 }
