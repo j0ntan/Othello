814c1655c098f9d99ff7f60e3b1136a6986fd8bb
j0ntan <jgamboa0209@gmail.com>
2018-11-01 03:00:14 -0400

    Implement recursive search

--- core/SimpleAI.cpp
+++ core/SimpleAI.cpp
@@ -7,7 +7,40 @@ namespace {
 int eval(const OthelloGameState *s, const OthelloCell &choosersTiles) {}
 
 int search(OthelloGameState *s, int depth, bool myTurn,
-           const OthelloCell &choosersTiles) {}
+           const OthelloCell &choosersTiles) {
+  if (depth == 0)
+    return eval(s, choosersTiles);
+  else {
+    if (myTurn) {
+      int max_score = -64;
+      for (int x = 0; x < s->board().width(); ++x)
+        for (int y = 0; y < s->board().height(); ++y)
+          if (s->isValidMove(x, y)) {
+            std::unique_ptr<OthelloGameState> clone = s->clone();
+            clone->makeMove(x, y);
+            int score = search(clone.get(), depth - 1, !myTurn, choosersTiles);
+            max_score = std::max(score, max_score);
+          }
+      if (max_score == -64)
+        return eval(s, choosersTiles);
+      else
+        return max_score;
+    } else {
+      int min_score = 64;
+      for (int x = 0; x < s->board().width(); ++x)
+        for (int y = 0; y < s->board().height(); ++y)
+          if (s->isValidMove(x, y)) {
+            s->makeMove(x, y);
+            int score = search(s, depth - 1, !myTurn, choosersTiles);
+            min_score = std::min(score, min_score);
+          }
+      if (min_score == 64)
+        return eval(s, choosersTiles);
+      else
+        return min_score;
+    }
+  }
+}
 } // namespace
 
 std::pair<int, int> SimpleAI::chooseMove(const OthelloGameState &state) {
