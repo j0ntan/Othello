bffdf62af5eda5b8838621522e6df04ff45ff925
j0ntan <jgamboa0209@gmail.com>
2018-11-01 03:33:27 -0400

    Create starting position board

--- exp/mainPlayGame.cpp
+++ exp/mainPlayGame.cpp
@@ -2,4 +2,16 @@
 #include "core/GameState.hpp"
 #include "core/SimpleAI.hpp"
 
-int main() {}
+// function prototypes
+Board startingPositionBoard();
+
+int main() { Board board = startingPositionBoard(); }
+
+Board startingPositionBoard() {
+  Board board;
+  board.setCellAt(3, 3, OthelloCell::white);
+  board.setCellAt(4, 3, OthelloCell::black);
+  board.setCellAt(3, 4, OthelloCell::black);
+  board.setCellAt(4, 4, OthelloCell::white);
+  return board;
+}
