01a2ebf85b977861e56b58e8938859b74f74413a
j0ntan <jgamboa0209@gmail.com>
2018-11-12 12:19:14 -0500

    Refactor to add const & remove unused vars

--- core/StrongerAI.cpp
+++ core/StrongerAI.cpp
@@ -8,6 +8,8 @@
 #include <vector>
 
 namespace {
+const int eval_range = 1000;
+
 bool compareMoves(const std::pair<int, int> &m1,
                   const std::pair<int, int> &m2) {
   return AI::quickMoveEval(m1) < AI::quickMoveEval(m2);
@@ -31,7 +33,7 @@ int search(OthelloGameState *s, int depth, bool myTurn,
     return AI::stronger::evaluate(s, choosersTiles);
   else {
     if (myTurn) {
-      int max_score = -64;
+      int max_score = -1 * eval_range;
       auto available_moves = findAvailableMoves(*s);
       std::sort(std::begin(available_moves), std::end(available_moves),
                 compareMoves);
@@ -39,21 +41,21 @@ int search(OthelloGameState *s, int depth, bool myTurn,
       for (const auto &move : available_moves) {
         const auto &x = move.first;
         const auto &y = move.second;
-        std::unique_ptr<OthelloGameState> clone = s->clone();
+        const std::unique_ptr<OthelloGameState> clone = s->clone();
         clone->makeMove(x, y);
-        int score =
+        const int score =
             search(clone.get(), depth - 1, !myTurn, choosersTiles, alpha, beta);
         max_score = std::max(score, max_score);
         alpha = std::max(alpha, max_score);
         if (beta <= alpha)
           return max_score;
       }
-      if (max_score == -64)
+      if (max_score == -1 * eval_range)
         return AI::stronger::evaluate(s, choosersTiles);
       else
         return max_score;
     } else {
-      int min_score = 64;
+      int min_score = eval_range;
       auto available_moves = findAvailableMoves(*s);
       std::sort(std::begin(available_moves), std::end(available_moves),
                 compareMoves);
@@ -61,16 +63,16 @@ int search(OthelloGameState *s, int depth, bool myTurn,
       for (const auto &move : available_moves) {
         const auto &x = move.first;
         const auto &y = move.second;
-        std::unique_ptr<OthelloGameState> clone = s->clone();
+        const std::unique_ptr<OthelloGameState> clone = s->clone();
         clone->makeMove(x, y);
-        int score =
+        const int score =
             search(clone.get(), depth - 1, !myTurn, choosersTiles, alpha, beta);
         min_score = std::min(score, min_score);
         beta = std::min(beta, min_score);
         if (beta <= alpha)
           return min_score;
       }
-      if (min_score == 64)
+      if (min_score == eval_range)
         return AI::stronger::evaluate(s, choosersTiles);
       else
         return min_score;
@@ -82,12 +84,10 @@ int search(OthelloGameState *s, int depth, bool myTurn,
 std::pair<int, int> StrongerAI::chooseMove(const OthelloGameState &state) {
   std::srand(std::time(nullptr));
   const int depth = 7;
-  const int width = state.board().width();
-  const int height = state.board().height();
   const OthelloCell currentPlayerTiles =
       state.isWhiteTurn() ? OthelloCell::white : OthelloCell::black;
 
-  int best_move_x = 0, best_move_y = 0, best_score = -100;
+  int best_move_x = 0, best_move_y = 0, best_score = -1 * eval_range;
   auto available_moves = findAvailableMoves(state);
   std::sort(std::begin(available_moves), std::end(available_moves),
             compareMoves);
@@ -95,10 +95,10 @@ std::pair<int, int> StrongerAI::chooseMove(const OthelloGameState &state) {
   for (const auto &move : available_moves) {
     const auto &x = move.first;
     const auto &y = move.second;
-    std::unique_ptr<OthelloGameState> newState = state.clone();
+    const std::unique_ptr<OthelloGameState> newState = state.clone();
     newState->makeMove(x, y);
-    int score =
-        search(newState.get(), depth - 1, false, currentPlayerTiles, -64, 64);
+    const int score = search(newState.get(), depth - 1, false,
+                             currentPlayerTiles, -1 * eval_range, eval_range);
     if (score > best_score || (score == best_score && std::rand() % 2)) {
       best_score = score;
       best_move_x = x;
